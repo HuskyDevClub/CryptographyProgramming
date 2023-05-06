c1d6e66f9dbf7dcf6585ce45ae0684a3cea29d67
AnyLeftovers <37920527+yudonglin@users.noreply.github.com>
2023-05-05 17:30:56 -0700

    add readme and split the functionalities as describe

--- CryptographicApp/src/ECDHIES.java
+++ CryptographicApp/src/ECDHIES.java
@@ -5,7 +5,12 @@ public class ECDHIES {
     final static int Z_LEN = 64;
     final static int T_LEN = 64;
 
-    static byte[] encrypt(byte[] data, byte[] pw) throws NoSuchAlgorithmException {
+    /**
+     * @param data the date that will be encrypted
+     * @param pw   the passphrase used for encryption
+     * @return the data that is encrypted
+     */
+    static byte[] encrypt(final byte[] data, final byte[] pw) throws NoSuchAlgorithmException {
         byte[] z = Glossary.random(Z_LEN * 8);
         var ke_ka = KMACX.KMACXOF256(Glossary.array_concatenation(z, pw), new byte[]{}, 1024, "S");
         byte[] ke = Glossary.substring(ke_ka, 0, ke_ka.length / 2);
@@ -20,13 +25,16 @@ public class ECDHIES {
         return Glossary.array_concatenation(z, c, t);
     }
 
-    static byte[] decrypt(byte[] data, byte[] pw) throws NoSuchAlgorithmException {
-        byte[] z = new byte[Z_LEN];
-        System.arraycopy(data, 0, z, 0, z.length);
-        byte[] c = new byte[data.length - Z_LEN - T_LEN];
-        System.arraycopy(data, z.length, c, 0, c.length);
-        byte[] t = new byte[T_LEN];
-        System.arraycopy(data, z.length + c.length, t, 0, t.length);
+    /**
+     * @param data the date that will be decrypted
+     * @param pw   the passphrase used for decryption
+     * @return the data that is decrypted
+     */
+    static byte[] decrypt(final byte[] data, final byte[] pw) throws NoSuchAlgorithmException {
+        // obtain z, c and t from data
+        byte[] z = Arrays.copyOfRange(data, 0, Z_LEN);
+        byte[] c = Arrays.copyOfRange(data, z.length, data.length - T_LEN);
+        byte[] t = Arrays.copyOfRange(data, z.length + c.length, data.length);
 
         byte[] ke_ka = KMACX.KMACXOF256(Glossary.array_concatenation(z, pw), new byte[]{}, 1024, "S");
         byte[] ke = Glossary.substring(ke_ka, 0, ke_ka.length / 2);
@@ -38,7 +46,7 @@ public class ECDHIES {
         }
         var t_inv = KMACX.KMACXOF256(ka, m, 512, "SKA");
         if (!Arrays.equals(t_inv, t)) {
-            throw new IllegalArgumentException("Invalid z!");
+            throw new IllegalArgumentException("Invalid z potentially due to incorrect passphrase!");
         }
         return m;
     }
--- CryptographicApp/src/Main.java
+++ CryptographicApp/src/Main.java
@@ -1,60 +1,138 @@
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
-        testAll();
         if (args.length < 1) {
-            Scanner _SCANNER = new Scanner(System.in);
-            System.out.print("Please enter a string: ");
-            String strIn = _SCANNER.nextLine();
-            System.out.println("string S: " + strIn);
-            process(strIn.getBytes());
-        } else if (args.length < 2) {
             System.out.println("Invalid argument format detected, abort.");
-        } else if (args[0].startsWith("-f")) {
-            process(Files.readAllBytes(Paths.get(args[1])));
-        } else if (args[0].startsWith("-s")) {
-            process(args[1].getBytes());
+        } else if (args[0].equals("-test")) {
+            // debugging use only
+            Glossary.test();
+            cShake.test();
         } else {
-            System.out.println("Invalid argument detected, abort.");
+            /* getting the input arguments */
+            final byte[] data;
+            final var argsL = Arrays.asList(args);
+            int data_index = argsL.indexOf("-f");
+            if (data_index < 0) {
+                data_index = argsL.indexOf("-s");
+                if (data_index < 0) {
+                    data = input("Please enter a string: ");
+                } else {
+                    data = args[data_index + 1].getBytes();
+                }
+                System.out.println("string S: " + new String(data));
+            } else {
+                data = Files.readAllBytes(Paths.get(args[data_index + 1]));
+            }
+
+            /* doing whatever the user is asking for */
+            if (args[0].equals("-h")) {
+                computeHash(data);
+            } else {
+                switch (args[0]) {
+                    case "-t" -> computeTag(data, getPassphrase(argsL));
+                    case "-e" -> encryptData(data, getPassphrase(argsL), getOutputPath(argsL));
+                    case "-d" -> decryptData(data, getPassphrase(argsL), getOutputPath(argsL));
+                    default -> throw new IllegalArgumentException("Invalid argument, abort.");
+                }
+            }
         }
     }
 
-    static private void process(byte[] data) throws NoSuchAlgorithmException {
-        // Computing a cryptographic hash
+    /**
+     * ask user to input sth
+     *
+     * @param message the message used to prompt for user input
+     * @return the user input in bytes
+     */
+    private static byte[] input(String message) {
+        System.out.print(message);
+        Scanner _SCANNER = new Scanner(System.in);
+        String strIn = _SCANNER.nextLine();
+        return strIn.getBytes();
+    }
+
+    /**
+     * get the passphrase
+     *
+     * @param args the input arguments
+     * @return the passphrase
+     */
+    private static byte[] getPassphrase(List<String> args) {
+        final int _index = args.indexOf("-p");
+        return _index < 0 ? input("Please enter a string as passphrase: ") : args.get(_index + 1).getBytes();
+    }
+
+    /**
+     * get the output path
+     *
+     * @param args the input arguments
+     * @return the output path
+     */
+    private static Path getOutputPath(List<String> args) {
+        final int _index = args.indexOf("-o");
+        return _index < 0 ? null : Path.of(args.get(_index + 1));
+    }
+
+    /**
+     * Computing a cryptographic hash
+     *
+     * @param data the data used to compute
+     */
+    private static void computeHash(byte[] data) throws NoSuchAlgorithmException {
         var h = KMACX.KMACXOF256("".getBytes(), data, 512, "D");
         System.out.printf("Plain cryptographic hash (length %d):\n", h.length);
         Glossary.displayBytes(h);
+    }
 
-        //Compute an authentication tag
-        Scanner _SCANNER = new Scanner(System.in);
-        System.out.print("Please enter a string as passphrase: ");
-        byte[] pw = _SCANNER.nextLine().getBytes();
+    /**
+     * Compute an authentication tag
+     *
+     * @param data the data used to compute
+     * @param pw   the passphrase that will be used
+     */
+    private static void computeTag(byte[] data, byte[] pw) throws NoSuchAlgorithmException {
         var t = KMACX.KMACXOF256(pw, data, 512, "T");
         System.out.printf("Authentication tag (length %d):\n", t.length);
         Glossary.displayBytes(t);
+    }
 
-        // Encrypting a byte array
+    /**
+     * encrypt given byte array data
+     *
+     * @param data the data that will be encrypted
+     * @param pw   the passphrase that will be used
+     */
+    private static void encryptData(byte[] data, byte[] pw, Path savedTo) throws NoSuchAlgorithmException, IOException {
         var enc_data = ECDHIES.encrypt(data, pw);
         System.out.printf("Encrypted data (length %d):\n", enc_data.length);
         Glossary.displayBytes(enc_data);
+        if (savedTo != null) {
+            Files.write(savedTo, enc_data);
+        }
+    }
 
-        // Decrypting a symmetric cryptogram
+    /**
+     * decrypt a symmetric cryptogram
+     *
+     * @param enc_data the data that will be decrypted
+     * @param pw       the passphrase that will be used
+     */
+    private static void decryptData(byte[] enc_data, byte[] pw, Path savedTo) throws NoSuchAlgorithmException, IOException {
         var dec_data = ECDHIES.decrypt(enc_data, pw);
         System.out.printf("\nDecrypted data (length %d):\n", dec_data.length);
         Glossary.displayBytes(dec_data);
         System.out.printf("Decrypted message: %s\n", new String(dec_data));
-        System.out.printf("Same as previous: %b\n", Arrays.equals(data, ECDHIES.decrypt(enc_data, pw)));
-    }
-
-    private static void testAll() throws NoSuchAlgorithmException {
-        Glossary.test();
-        cShake.test();
+        if (savedTo != null) {
+            Files.write(savedTo, dec_data);
+        }
     }
 }
